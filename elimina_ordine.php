e7e8559d667784327f9f5671c2d90d83f0108e20
alessandropiazzalunga <piazzalunga.alessandro.07@itisdalmine.edu.it>
2026-05-11 13:01:47 +0200

    Implement order deletion functionality

--- elimina_ordine.php
+++ elimina_ordine.php
@@ -1,1 +1,20 @@
+<?php
+require 'config.php';
+session_start();
 
+if(!isset($_SESSION['ruolo']) || $_SESSION['ruolo'] != 'root')
+{
+    die("Accesso negato");
+}
+
+if(isset($_GET['id']))
+{
+    $id = $_GET['id'];
+
+    $sql = "DELETE FROM ordini WHERE id = ?";
+    $stm = $pdo->prepare($sql);
+    $stm->execute([$id]);
+}
+
+header("Location: home.php");
+exit();
