2f05838835ae30d1955ab8ddaa9527106829b22f
alessandropiazzalunga <piazzalunga.alessandro.07@itisdalmine.edu.it>
2026-05-12 16:17:33 +0200

    add ACID property
    Added transaction handling with error management for deleting orders.

--- elimina_ordine.php
+++ elimina_ordine.php
@@ -2,17 +2,31 @@
 require 'config.php';
 session_start();
 
-if(!isset($_SESSION['ruolo']) || $_SESSION['ruolo'] != 'root')
+if (!isset($_SESSION['ruolo']) || $_SESSION['ruolo'] != 'root')
 {
     die("Accesso negato");
 }
 
-if(isset($_GET['id']))
+if (isset($_GET['id']))
 {
-    $id = $_GET['id'];
-    $sql = "DELETE FROM ordini WHERE id = ?";
-    $stm = $pdo->prepare($sql);
-    $stm->execute([$id]);
+    try
+    {
+        $pdo->exec("SET SESSION innodb_lock_wait_timeout = 10");
+        $pdo->exec("SET autocommit = 0");
+        $pdo->exec("BEGIN WORK");
+
+        $id = $_GET['id'];
+        $sql = "DELETE FROM ordini WHERE id = ?";
+        $stm = $pdo->prepare($sql);
+        $stm->execute([$id]);
+        $pdo->exec("COMMIT");
+    }
+    catch (Exception $e)
+    {
+        $pdo->exec("ROLLBACK");
+
+        die("Errore: " . $e->getMessage());
+    }
 }
 
 header("Location: home.php");
